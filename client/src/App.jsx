a7c96f420fe242ffde7d50f01cdb3ba6fa6f486d
Nayan Sharma <nayan@Nayans-MacBook-Air.local>
2024-12-02 00:08:14 +0530

    Add react-router with pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
 const App = () => {
-  return <>ZapChat</>;
+  return (
+    <Routes>
+      <Route path="/" element={<ChatPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default App;

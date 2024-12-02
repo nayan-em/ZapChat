c979c566e5eec5f3f9d9ffae49aa4f3cd708efa6
Nayan Sharma <nayan@Nayans-MacBook-Air.local>
2024-12-02 21:16:05 +0530

    Add structure for LoginPage component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <nav className="bg-white border-gray-200 dark:bg-black dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
         <NavLink
           to="/"

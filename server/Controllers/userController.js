c3a55f88d70ce5fb3a890f8ddda366b8817145ce
Nayan Sharma <nayan@Nayans-MacBook-Air.local>
2024-12-09 18:02:07 +0530

    Add checks for register user

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,8 +1,25 @@
 const userModel = require("../Models/userModel");
+const validator = require("validator");
 
-const registerUser = (req, res) => {
-  res.send("Register");
-  console.log("Register");
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await userModel.findOne({ email });
+
+    if (user)
+      return res.status(400).json("User with this email already exists...");
+
+    if (!validator.isEmail(email))
+      return res.status(400).json("Email must be a valid email...");
+
+    if (!validator.isStrongPassword(password))
+      return res.status(400).json("Password is not strong enough...");
+
+    user = new userModel({ name, email, password });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
 module.exports = { registerUser };

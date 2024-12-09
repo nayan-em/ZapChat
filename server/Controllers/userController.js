ff8ef6920eaa7f9e6fddf8620e8ad20c125d9cff
Nayan Sharma <nayan@Nayans-MacBook-Air.local>
2024-12-09 19:12:59 +0530

    Add JWT token and password encryption

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,5 +1,12 @@
 const userModel = require("../Models/userModel");
+const bcrypt = require("bcrypt");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  const jwtKey = process.env.JWT_SECRET_KEY;
+  return jwt.sign({ _id }, jwtKey, { expiresIn: "3d" });
+};
 
 const registerUser = async (req, res) => {
   try {
@@ -16,6 +23,16 @@ const registerUser = async (req, res) => {
       return res.status(400).json("Password is not strong enough...");
 
     user = new userModel({ name, email, password });
+
+    // Encrypt password using 10-bit encryption
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+
+    // Create jwt token and send it to client
+    const token = createToken(user._id);
+    res.status(200).json({ _id: user._id, name, email, token });
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
